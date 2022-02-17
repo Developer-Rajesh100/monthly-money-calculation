4965b5822de0f202aed92b4d9db11d0c2bb0a04f
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-02-17 13:08:41 +0530

    Complete whitout save

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,36 @@
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const income = document.getElementById("monthly-income").value;
-  const foodCost = document.getElementById("food-cost").value;
-  const rentCost = document.getElementById("rent-cost").value;
-  const clothesCost = document.getElementById("clothes-cost").value;
+  const income = document.getElementById("monthly-income");
+  const monthlyIncome = income.value;
+  const foodCost = document.getElementById("food-cost");
+  const monthlyFoodCost = foodCost.value;
+  const rentCost = document.getElementById("rent-cost");
+  const monthlyRentCost = rentCost.value;
+  const clothesCost = document.getElementById("clothes-cost");
+  const monthlyClothesCost = clothesCost.value;
+  //   ALL COST PLUS & PRINT
   const total =
-    parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
+    parseFloat(monthlyFoodCost) +
+    parseFloat(monthlyRentCost) +
+    parseFloat(monthlyClothesCost);
   document.getElementById("expenses").innerText = total;
   console.log(total);
-  const monthlyBalance = income - total;
+  //   TOTAL BALANCE MINUS ALL COST
+  const monthlyBalance = monthlyIncome - total;
   document.getElementById("balance").innerText = monthlyBalance;
   console.log(monthlyBalance);
+  //   CLEAR INPUT BOX
+  income.value = "";
+  foodCost.value = "";
+  rentCost.value = "";
+  clothesCost.value = "";
+  //   ERROR HANDELING
+  if (
+    monthlyIncome < 0 ||
+    monthlyFoodCost < 0 ||
+    monthlyRentCost < 0 ||
+    monthlyClothesCost < 0 ||
+    monthlyIncome < total
+  ) {
+    alert("Please Input Valide Value");
+  }
 });
